fffc5bdb91d0fe990f6cb3b39eec8c33f610e22e
Marco Schneider <marco.schneider@active-group.de>
2025-10-27 19:08:18 +0100

    infra: Add cloudflare astro adapter.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
   site: 'https://defmarco.com',
   integrations: [tailwind()],
   output: 'static',
+
   i18n: {
     defaultLocale: 'de',
     locales: ['de', 'en'],
-  }
+  },
+
+  adapter: cloudflare()
 });
