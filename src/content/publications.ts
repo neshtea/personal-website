697b32b4f440167f30227881c078de4a47d1f33c
Marco Schneider <marco.schneider@active-group.de>
2025-10-28 09:07:41 +0100

    content: Add publications.

---
+++ src/content/publications.ts
@@ -0,0 +1,65 @@
+export const publications = [
+  {
+    title: "Bidirectional Data Transformations",
+    authors: "Crestani, M. Schlegel, M., Schneider, M.",
+    venue:
+      "Proceedings of the 2nd ACM SIGPLAN International Workshop on Functional Software Architecture (pp. 31-41)",
+    links: {
+      ACM: "https://dl.acm.org/doi/10.1145/3677998.3678224",
+      ICFP24:
+        "https://icfp24.sigplan.org/details/funarch-2024-papers/6/Bidirectional-Data-Transformations?ref=defmarco.com",
+    },
+  },
+];
+
+export const workshops = [
+  {
+    title: "Funktionale Programmierung -- Einführung",
+    venue: "Rheinwerk KKon",
+    location: "online",
+    date: "September 2024",
+    links: {},
+  },
+  {
+    title: "Funktionale Programmierung -- Einführung",
+    venue: "Rheinwerk KKon",
+    location: "online",
+    date: "September 2023",
+    links: {},
+  },
+];
+
+export const talks = [
+  {
+    title: "Hyper Hyper! Javascript fatigue und die Hypermedia Renaissance (?)",
+    event: "DevDay",
+    location: "Dresden, Germany",
+    date: "Mai 2024",
+    links: {},
+  },
+  {
+    title: "Hyper Hyper! Javascript fatigue und die Hypermedia Renaissance (?)",
+    event: "BOB Konferenz",
+    location: "Berlin, Germany",
+    date: "März 2024",
+    links: {
+      event: "https://bobkonf.de/2024/schneider.html?ref=defmarco.com",
+      video: "https://www.youtube.com/watch?v=cp52TBEW76E&ref=defmarco.com",
+    },
+  },
+  {
+    title: "Wie man mithilfe hoher Kopplung Softwareprojekte sabotiert",
+    event: "DevDay",
+    location: "Dresden, Germany",
+    date: "Mai 2023",
+    links: {},
+  },
+  {
+    title:
+      "Die ganze Geschichte ein 'scanl' entfernt -- Event Sourcing in Clojure",
+    event: "DevDay",
+    location: "Dresden, Germany",
+    date: "Mai 2023",
+    links: {},
+  },
+];
