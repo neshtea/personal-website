13efcc7e5502184f460602f0ecea038cc18e8dcc
Marco Schneider <marco.schneider@active-group.de>
2026-02-21 23:08:35 +0100

    checkpoint

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://defmarco.com',
-  integrations: [tailwind()],
+  integrations: [],
   output: 'static',
 
   i18n: {
